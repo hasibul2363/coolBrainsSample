5c1a43c8f4e643c4a75f84bb8921fe4c3d08fe9f
Hasibul Haque <hasibul2363@gmail.com>
2018-11-30 01:53:57 +0600

    inbetween dev

---
+++ src/back-end/businessApp/Commands/CreateProductCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SuitSupply.Infrastructure.Bus.Contracts.Command;
+
+namespace SuitSupply.ProductCatalog.Commands
+{
+    public class CreateProductCommand : SuitCommand
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/back-end/businessApp/Validators/CreateProductCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using SuitSupply.Infrastructure.Validator.Contract;
+using SuitSupply.Infrastructure.Validator.Utility;
+using SuitSupply.ProductCatalog.Commands;
+
+namespace SuitSupply.ProductCatalog.Validators
+{
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>, ISuitValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(p => p.Id).NotEmpty();
+            RuleFor(p => p.Code).NotEmpty().Must(Unique);
+            RuleFor(p => p.Price).NotEmpty().GreaterThan(0);
+            RuleFor(p => p.Name).NotEmpty();
+        }
+
+        private bool Unique(string productCode)
+        {
+            return true;
+        }
+
+        public SuitValidationResult PerformValidation(CreateProductCommand command)
+        {
+            return Validate(command).ToSuitValidationResult();
+        }
+    }
+}
