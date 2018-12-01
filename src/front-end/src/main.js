910b47fdbafe879b11598fd6aaefc3dd54c35229
Hasibul Haque <hasibul2363@gmail.com>
2018-12-01 13:00:04 +0600

    added muse UI

--- src/front-end/src/main.js
+++ src/front-end/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
+
 
 Vue.config.productionTip = false
+Vue.use(MuseUI);
 
 new Vue({
   router,
--- src/front-end/src/views/About.vue
+++ src/front-end/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <mu-button color="primary">Primary</mu-button>
   </div>
 </template>
