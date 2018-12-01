c64b34965f15fd66cbf0a74dc87b78d4e3fb0bb1
Hasibul Haque <hasibul2363@gmail.com>
2018-12-01 13:41:28 +0600

    added Navbar

---
+++ src/front-end/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <mu-appbar style="width: 100%;" color="primary" title="SuitSupply">
+      <mu-button flat slot="right" @click="navigateToHome">Home</mu-button>
+      <mu-button flat slot="right" @click="navigateToProducts">Products</mu-button>
+    </mu-appbar>
+  </div>
+</template>
+
+<script>
+import router from "../router";
+export default {
+  name: "Navbar",
+  methods: {
+    navigateToHome: () => {
+      router.push({ path: "/" });
+    },
+    navigateToProducts: () => {
+      router.push({ path: "/products" });
+    }
+  }
+};
+</script>
